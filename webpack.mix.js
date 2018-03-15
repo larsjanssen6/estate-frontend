19a2cb1b80a86b472bb2a306e02bb425dee61cd7
Alex <alex_kir@live.nl>
2018-03-15 14:15:04 +0100

    update webpack.mix.js for windows
    fix for 95% emitting error

--- webpack.mix.js
+++ webpack.mix.js
@@ -2,7 +2,7 @@ const mix = require('laravel-mix');
 
 var tailwindcss = require('tailwindcss');
 
-
+mix.setPublicPath("./");
 mix.js('resources/js/app.js', 'public/js');
 
 mix.postCss('resources/css/main.css', 'public/css', [
