2ea3f287493528f57840ff48e4764110691781a2
Lars Janssen <larsjanssen64@gmail.com>
2018-04-11 11:07:50 +0200

    Not used

--- resources/js/components/Register.vue
+++ resources/js/components/Register.vue
@@ -65,7 +65,7 @@
 			
 			<div class="mb-6">
                 <label class="label mb-2">Interesse vanaf:</label>
-                <datetime type="datetime" placeholder="Selecteer datum" v-model="form.interestdate" required>Klik hier om een keuze te maken.</datetime>
+                <datetime placeholder="Selecteer datum" v-model="form.interestdate" required>Klik hier om een keuze te maken.</datetime>
             </div>
 
             <div class="mb-6">
