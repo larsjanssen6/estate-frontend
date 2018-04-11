6fd98f71917d24092f65d0da3bf79fe44efa1148
Lars Janssen <larsjanssen64@gmail.com>
2018-04-11 11:51:30 +0200

    Compile

--- public/js/app.js
+++ public/js/app.js
@@ -1687,7 +1687,6 @@ Object.defineProperty(__webpack_exports__, "__esModule", { value: true });
 //
 //
 //
-//
 
 
 
@@ -1717,9 +1716,6 @@ Object.defineProperty(__webpack_exports__, "__esModule", { value: true });
 
 
     methods: {
-        goToNote: function goToNote() {
-            this.$router.push('/notities');
-        },
         showUser: function showUser(user) {
             Bus.$emit('show-user', user);
         },
@@ -1840,11 +1836,18 @@ Object.defineProperty(__webpack_exports__, "__esModule", { value: true });
             __WEBPACK_IMPORTED_MODULE_0__axios__["a" /* default */].post('token/generate-token', this.creds).then(function (_ref) {
                 var data = _ref.data;
 
-                _this.$store.dispatch("login", true);
+
                 localStorage.setItem('token', data.token);
+                _this.getUser();
                 _this.$router.push('/home');
             }).catch(function (error) {
                 _this.wrong = true;
+            });
+        },
+        getUser: function getUser() {
+            __WEBPACK_IMPORTED_MODULE_0__axios__["a" /* default */].get('/user').then(function (response) {
+                alert('dfddd');
+                Bus.$emit('userHasLoggedIn', response.data);
             });
         }
     }
@@ -29672,11 +29675,7 @@ var render = function() {
               _c(
                 "datetime",
                 {
-                  attrs: {
-                    type: "datetime",
-                    placeholder: "Selecteer datum",
-                    required: ""
-                  },
+                  attrs: { placeholder: "Selecteer datum", required: "" },
                   model: {
                     value: _vm.form.interestdate,
                     callback: function($$v) {
@@ -29893,12 +29892,12 @@ var render = function() {
               )
             ]),
             _vm._v(" "),
-            _c("div", { staticClass: "mb-6 flex flex-col" }, [
+            _c("div", { staticClass: "flex flex-col" }, [
               _c("div", [
-                _c("label", { staticClass: "label mb-2" }, [_vm._v("Rol")])
+                _c("label", { staticClass: "label" }, [_vm._v("Rol")])
               ]),
               _vm._v(" "),
-              _c("div", { staticClass: "flex" }, [
+              _c("div", { staticClass: "flex mb-2 mt-2" }, [
                 _c(
                   "span",
                   {
@@ -30210,22 +30209,8 @@ var render = function() {
       _c(
         "button",
         {
-          staticClass: "btn-normal",
-          attrs: { type: "button" },
-          on: {
-            click: function($event) {
-              _vm.goToNote()
-            }
-          }
-        },
-        [_vm._v("Ga naar notities")]
-      ),
-      _vm._v(" "),
-      _c(
-        "button",
-        {
           staticClass:
-            "bg-teal font-semibold text-white py-2 px-4 border border-teal hover:border-transparent rounded m-4",
+            "bg-teal font-semibold text-white py-2 px-4 border border-teal hover:border-transparent rounded mt-4 mb-4",
           on: {
             click: function($event) {
               _vm.$modal.show("registration")
@@ -30247,75 +30232,88 @@ var render = function() {
           _c(
             "tbody",
             _vm._l(_vm.users, function(user) {
-              return _c("tr", { staticClass: "hover:bg-blue-lightest" }, [
-                _c("td", { staticClass: "tr" }, [
-                  _vm._v(_vm._s(user.first_name))
-                ]),
-                _vm._v(" "),
-                _c("td", { staticClass: "tr" }, [_vm._v(_vm._s(user.surname))]),
-                _vm._v(" "),
-                _c("td", { staticClass: "tr" }, [_vm._v(_vm._s(user.city))]),
-                _vm._v(" "),
-                _c("td", { staticClass: "tr" }, [
-                  _vm._v(_vm._s(user.date_joined))
-                ]),
-                _vm._v(" "),
-                _c("td", { staticClass: "tr" }, [
-                  _c(
-                    "button",
-                    {
-                      staticClass: "btn-normal",
-                      attrs: { type: "button" },
-                      on: {
-                        click: function($event) {
-                          _vm.showUser(user)
-                        }
-                      }
-                    },
-                    [
-                      _vm._v(
-                        "\n                        Bekijken\n                    "
-                      )
-                    ]
-                  ),
+              return _c(
+                "tr",
+                {
+                  staticClass: "hover:bg-blue-lightest cursor-pointer",
+                  on: {
+                    click: function($event) {
+                      _vm.showUser(user)
+                    }
+                  }
+                },
+                [
+                  _c("td", { staticClass: "tr" }, [
+                    _vm._v(_vm._s(user.first_name))
+                  ]),
                   _vm._v(" "),
-                  _c(
-                    "button",
-                    {
-                      staticClass: "btn-delete",
-                      attrs: { type: "button" },
-                      on: {
-                        click: function($event) {
-                          _vm.deleteUser(user)
-                        }
-                      }
-                    },
-                    [
-                      _vm._v(
-                        "\n                        Verwijderen\n                    "
-                      )
-                    ]
-                  ),
+                  _c("td", { staticClass: "tr" }, [
+                    _vm._v(_vm._s(user.surname))
+                  ]),
                   _vm._v(" "),
-                  _c(
-                    "button",
-                    {
-                      staticClass: "btn-normal",
-                      attrs: { type: "button" },
-                      on: {
-                        click: function($event) {
-                          _vm.promoteUser(user)
+                  _c("td", { staticClass: "tr" }, [_vm._v(_vm._s(user.city))]),
+                  _vm._v(" "),
+                  _c("td", { staticClass: "tr" }, [
+                    _vm._v(_vm._s(user.date_joined))
+                  ]),
+                  _vm._v(" "),
+                  _c("td", { staticClass: "tr" }, [
+                    _c(
+                      "button",
+                      {
+                        staticClass: "btn-normal",
+                        attrs: { type: "button" },
+                        on: {
+                          click: function($event) {
+                            _vm.showUser(user)
+                          }
                         }
-                      }
-                    },
-                    [
-                      _vm._v(
-                        "\n                        Admin\n                    "
-                      )
-                    ]
-                  )
-                ])
-              ])
+                      },
+                      [
+                        _vm._v(
+                          "\n                        Bekijken\n                    "
+                        )
+                      ]
+                    ),
+                    _vm._v(" "),
+                    _c(
+                      "button",
+                      {
+                        staticClass: "btn-delete",
+                        attrs: { type: "button" },
+                        on: {
+                          click: function($event) {
+                            _vm.deleteUser(user)
+                          }
+                        }
+                      },
+                      [
+                        _vm._v(
+                          "\n                        Verwijderen\n                    "
+                        )
+                      ]
+                    ),
+                    _vm._v(" "),
+                    _c(
+                      "button",
+                      {
+                        staticClass: "btn-normal",
+                        attrs: { type: "button" },
+                        on: {
+                          click: function($event) {
+                            _vm.promoteUser(user)
+                          }
+                        }
+                      },
+                      [
+                        _vm._v(
+                          "\n                        Admin\n                    "
+                        )
+                      ]
+                    )
+                  ])
+                ]
+              )
             })
           )
         ]
@@ -47426,9 +47424,21 @@ var app = new Vue({
     router: router,
     store: __WEBPACK_IMPORTED_MODULE_6__store__["a" /* default */],
 
+    created: function created() {
+        var _this = this;
+
+        Bus.$on('userHasLoggedIn', function (user) {
+            _this.$store.commit('setUser', user);
+        });
+    },
+
+
     methods: {
         goHome: function goHome() {
             window.location = '/';
+        },
+        setUser: function setUser(user) {
+            this.store.commit('setUser', user);
         }
     }
 }).$mount('#app');
@@ -47889,13 +47899,7 @@ var store = new __WEBPACK_IMPORTED_MODULE_1_vuex__["a" /* default */].Store({
         logout: function logout(state) {
             localStorage.removeItem('token');
             state.authenticated = false;
-        }
-    },
-
-    actions: {
-        logout: function logout(context) {
-            alert('logout');
-            context.commit('logout');
+            state.user = { role: 1 };
         }
     }
 });
